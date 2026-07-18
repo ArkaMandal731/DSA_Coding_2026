fe8434f7b6e9f1855395dbae5cc7d5743e45047c
Arka Mandal <109474507+ArkaMandal731@users.noreply.github.com>
2026-07-18 14:36:02 +0000

    Code fixes

--- reverseWord.java
+++ reverseWord.java
@@ -3,7 +3,7 @@ public class reverseWord {
 
     public static void main(String[] args) {
 
-        String str = "Arka is a good boy";
+        String str = "Arka";
         String rev = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
