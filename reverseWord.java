ff2b3e43d1e1e817d88ddcd8d3cea493e19bff10
Arka Mandal <109474507+ArkaMandal731@users.noreply.github.com>
2026-07-18 16:18:40 +0000

    Fixes for 1st code

--- reverseWord.java
+++ reverseWord.java
@@ -7,7 +7,7 @@ public class reverseWord {
         String rev = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
-            rev += str.charAt(i);
+            rev=rev+ str.charAt(i);
         }
 
         System.out.println(rev);
